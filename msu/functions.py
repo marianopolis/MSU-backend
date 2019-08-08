845cd719f39a7d3776f45f4cac53869d42e30be5
TheMatrixMaster <stephen.lu.2002@gmail.com>
2019-08-07 22:20:50 -0400

    Inplement retrieve, upload and delete

--- msu/functions.py
+++ msu/functions.py
@@ -2,12 +2,9 @@ import datetime
 
 def get_posts(get_archived=False):
     """retrieve archived or unarchived posts from database"""
-    if get_archived:
-        archived_posts = Posts.query.filter_by(archived=True).order_by(desc(Posts.uploadtime)).all()
-        return archived_posts
-    else:
-        unarchived_posts = Posts.query.filter_by(archived=False).order_by(desc(Posts.uploadtime)).all()
-        return unarchived_posts
+    if get_archived: posts = Posts.query.filter_by(archived=True).order_by(desc(Posts.uploadtime)).all()
+    else: posts = Posts.query.filter_by(archived=False).order_by(desc(Posts.uploadtime)).all()
+    return posts
 
 def create_post(title, text, author):
     """create new post class and add it to the database"""
@@ -19,25 +16,51 @@ def create_post(title, text, author):
     db.session.add(new_post)
     db.session.commit()
 
+def delete_post(id_):
+    post_to_delete = Posts.query.get(id_)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+
 def get_documents():
     """retrieve all documents from database with corresponding description and download link"""
     documents = Files.query.order_by(desc(Files.uploadtime)).all()
     return documents
+    
 
-def upload_document(file, filename, description, author, uploadtime):
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def upload_document(file, filename, description, author):
     # store document data in file hosting server and get file access url (file_url)
 
     new_document = Files(filename=filename, file_url=file_url, description=description, author=author, uploadtime=datetime.datetime.now())
     db.session.add(new_document)
     db.session.commit()
 
-def get_forms():
+def delete_document(id_):
+    file_to_delete = Files.query.get(id_)
+    file_url = file_to_delete.file_url
+
+    # delete document data from file hosting server using file url
+
+    db.session.delete(file_to_delete)
+    db.session.commit()
+
+def get_forms(private=False):
     """retrive all forms from database"""
-    forms = Forms.query.order_by(desc(Forms.uploadtime)).all()
+    if private: forms = Forms.query.filter_by(private=True).order_by(desc(Forms.uploadtime)).all()
+    else: forms = Forms.query.filter_by(private=False).order_by(desc(Forms.uploadtime)).all()
     return forms
 
 def upload_form(name, subject, message):
     """create new form object class and add to database"""
-    new_form = Forms(name=name, subject=subject, message=message, uploadtime=uploadtime)
+    new_form = Forms(name=name, subject=subject, message=message, uploadtime=datetime.datetime.now())
     db.session.add(new_form)
+    db.session.commit()
+
+def delete_form(id_):
+    form_to_delete = Forms.query.get(id_)
+    db.session.delete(form_to_delete)
     db.session.commit()
--- msu/view.py
+++ msu/view.py
@@ -3,12 +3,15 @@ from flask import (
     session,
     redirect,
     url_for,
+    flash,
     abort,
     request,
     render_template,
 )
+from werkzeug.utils import secure_filename
 
 from .models import Admins
+from .functions import *
 
 bp = Blueprint('view', __name__)
 
@@ -48,6 +51,27 @@ def newspage(username):
     GET  => retrieve and display all archived and unarchived posts
     POST => add new post to database and send push notification
     """
+    if request.method == 'POST':
+        formType = request.form['form_type']
+
+        if formType == 'add':
+            postTitle = request.form['title']
+            postAuthor = request.form['author']
+            postContent = request.form['text']
+            sendNotif = request.form['notif']
+
+            #create_post(postTitle, postContent, postAuthor)
+
+            if sendNotif == 'on':
+                pass # send push notifs to frontend
+
+        elif formType == 'delete':
+            postID = request.form['id']
+            #delete_post(postID)
+
+    #all_posts = get_posts() # unarchived posts
+
+
     if session.get('logged', None):
         return render_template('newspage.html', username=username)
     else:
@@ -59,6 +83,42 @@ def documentpage(username):
     GET  => retrieve and display all available documents from file hosting
     POST => add new document and info to file hosting and postgresql db
     """
+
+    if request.method == 'POST':
+        formType = request.form['form_type']
+
+        if formType == 'add':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            if file.filename == '':
+                flash('No file selected for uploading')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                fileName = (request.form['filename'].split('.')[0]) + '.' + (file.filename.split('.')[-1])
+                fileName = secure_filename(fileName)
+                fileDescription = request.form['description']
+                fileAuthor = request.form['author']
+                sendNotif = request.form['notif']
+
+                upload_document(file, fileName, fileDescription, fileAuthor)
+                flash('File successfully uploaded')
+
+                if sendNotif == 'on':
+                    pass # send push notifs to frontend
+            else:
+                flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+                return redirect(request.url)
+
+        elif formType == 'delete':
+            documentID = request.form['id']
+            delete_document(documentID)
+
+
+    #all_documents = get_documents()
+
     if session.get('logged', None):
         return render_template('documentpage.html', username=username)
     else:
@@ -69,7 +129,22 @@ def documentpage(username):
 def formpage(username):
     """
     GET => retrieve and display all available forms from postgresql db
+    POST => delete form
     """
+
+    if request.method == 'POST':
+        formID = request.form['id']
+        #delete_form(formID)
+
+    #user = Admins.get_by_username(session['username'])
+    #isAdmin = user.admin
+
+    #public_forms = get_forms(private=False)
+
+    #if isAdmin: private_forms = get_forms(private=True)
+    #else: private_forms = []
+
+
     if session.get('logged', None):
         return render_template('formpage.html', username=username)
     else:
