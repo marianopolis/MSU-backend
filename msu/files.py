4db9682ebb12c18a6341d82e0c4b1fb44cae67b6
Sheheryar Parvaz <skipper308@hotmail.ca>
2019-10-31 22:08:20 -0400

    Add developer documentation
    fixes #6

--- msu/files.py
+++ msu/files.py
@@ -1,3 +1,15 @@
+"""Utility module for file handling.
+
+Using AWS S3, we can upload and download files. To
+use this module, ensure boto3 is properly configured
+by following the instructions at
+
+[https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html].
+
+Additionally, ensure the relevant environment variables in `config.py`
+are set.
+"""
+
 from urllib import parse
 from flask import current_app
 import boto3
